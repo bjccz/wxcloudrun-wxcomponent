06df4d77e78ec08f0d1e6ec3daf113a0652449a5
bjccz <bj_ccz@163.com>
2023-10-08 17:07:08 +0800

    去掉白名单

--- middleware/innerservice.go
+++ middleware/innerservice.go
@@ -1,9 +1,6 @@
 package middleware
 
 import (
-	"net/http"
-
-	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/errno"
 	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/log"
 	"github.com/gin-gonic/gin"
 )
@@ -12,20 +9,20 @@ var IpWhitelist []string = []string{"127.0.0.1"}
 
 // 内部服务中间件
 func InnerServiceMiddleWare(c *gin.Context) {
-	whitelisted := true
+	//whitelisted := false
 	clientIp := getRequestIP(c)
 	log.Info("clientIp: ", clientIp)
-	for _, v := range IpWhitelist {
-		if v == clientIp {
-			whitelisted = true
-		}
-	}
-	if whitelisted {
-		c.Next()
-	} else {
-		c.Abort()
-		c.JSON(http.StatusUnauthorized, errno.ErrNotAuthorized)
-	}
+	//for _, v := range IpWhitelist {
+	//	if v == clientIp {
+	//		whitelisted = true
+	//	}
+	//}
+	//if whitelisted {
+	c.Next()
+	//} else {
+	//	c.Abort()
+	//	c.JSON(http.StatusUnauthorized, errno.ErrNotAuthorized)
+	//}
 }
 
 // 获取ip
